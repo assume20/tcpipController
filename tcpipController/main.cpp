f2847e856e6be123e2cce95acd355e289723301b
assume20 <473917563@qq.com>
2018-10-13 22:36:27 +0800

    111

--- tcpipController/main.cpp
+++ tcpipController/main.cpp
@@ -38,7 +38,7 @@ int main(int argc, char *argv[])
 		if (e == 777)
 		{
 
-			QProcess::startDetached(qApp->applicationFilePath(), QStringList());
+			//QProcess::startDetached(qApp->applicationFilePath(), QStringList());
 
 			return 0;
 
